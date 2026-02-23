cba1626a03da5bd58bd08686272ea5e40f653f88
khanhha2711 <khanhha27112003@gmail.com>
2026-02-23 10:30:28 +0700

    prepare for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,28 @@ import router from "./routes/taskRouters.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 //midlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV === "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 //router
 app.use("/api/tasks", router);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 //create PORT
 connectDB().then(() => {
